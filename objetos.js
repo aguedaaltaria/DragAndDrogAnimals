ef1b15515c59f154315d6b938411b03046653457
aguedaaltaria <156155201+aguedaaltaria@users.noreply.github.com>
2024-06-05 14:30:29 -0700

    yup
    yes

---
+++ objetos.js
@@ -0,0 +1,122 @@
+const animales = [
+    {
+        id: 1,
+        nombre: "perro",
+        imagen: "./img/perro.png",
+        emogi:"🐕"
+    },
+    {
+        id: 2,
+        nombre: "gato",
+        imagen: "./img/gato.png",
+        emogi:"🐈"
+    },
+    {
+        id: 3,
+        nombre: "vaca",
+        imagen: "./img/vaca.png",
+        emogi:"🐄"
+    },
+    {
+        id: 4,
+        nombre: "cerdo",
+        imagen: "./img/cerdo.png",
+        emogi:"🐖"
+    },
+    {
+        id: 5,
+        nombre: "gallina",
+        imagen: "./img/gallina.png",
+        emogi:"🐔"
+    },
+    {
+        id: 6,
+        nombre: "pato",
+        imagen: "./img/pato.png",
+        emogi:"🦆"
+    },
+    {
+        id: 7,
+        nombre: "pollito",
+        imagen: "./img/pollito.png",
+        emogi:"🐤"
+    },
+    {
+        id: 8,
+        nombre: "oveja",
+        imagen: "./img/oveja.png",
+        emogi:"🐑"
+    },
+    {
+        id: 9,
+        nombre: "leon",
+        imagen: "./img/leon.png",
+        emogi:"🦁"
+    },
+    {
+        id: 10,
+        nombre: "caballo",
+        imagen: "./img/caballo.png",
+        emogi:"🐎"
+    },
+    {
+        id: 11,
+        nombre: "elefante",
+        imagen: "./img/elefante.png",
+        emogi:"🐘"
+    },
+    {
+        id: 12,
+        nombre: "abeja",
+        imagen: "./img/abeja.png",
+        emogi:"🐝"
+    },
+    {
+        id: 13,
+        nombre: "serpiente",
+        imagen: "./img/serpiente.png",
+        emogi:"🐍"
+    },
+    {
+        id: 14,
+        nombre: "ballena",
+        imagen: "./img/ballena.png",
+        emogi:"🐳"
+    },
+    {
+        id: 15,
+        nombre: "tortuga",
+        imagen: "./img/tortuga.png",
+        emogi:"🐢"
+    },
+    {
+        id: 16,
+        nombre: "delfin",
+        imagen: "./img/delfin.png",
+        emogi:"🐬"
+    },
+    {
+        id: 17,
+        nombre: "araña",
+        imagen: "./img/arana.png",
+        emogi:"🕷️"
+    },
+    {
+        id: 18,
+        nombre: "pulpo",
+        imagen: "./img/pulpo.png",
+        emogi:"🐙"
+    },
+    {
+        id: 19,
+        nombre: "jirafa",
+        imagen: "./img/jirafa.png",
+        emogi:"🦒"
+    },
+    {
+        id: 20,
+        nombre: "conejo",
+        imagen: "./img/conejo.png",
+        emogi:"🐇"
+    }
+];
